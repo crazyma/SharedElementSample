117dc6514a9aa64505d434a5cd0b4f71be174355
David-Retina15 <david77115@gmail.com>
2017-12-14 16:03:13 +0800

    feat: add shared element effect in actactfrag

--- app/src/main/java/com/beibeilab/sharedelementsample/actactfrag/Activity201.kt
+++ app/src/main/java/com/beibeilab/sharedelementsample/actactfrag/Activity201.kt
@@ -2,9 +2,13 @@ package com.beibeilab.sharedelementsample.actactfrag
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.ActivityOptionsCompat
 import android.support.v7.app.AppCompatActivity
 import com.beibeilab.sharedelementsample.R
 import kotlinx.android.synthetic.main.activity_201.*
+import android.support.v4.util.Pair
+import android.view.View
 
 /**
  * Created by david on 2017/12/13.
@@ -15,8 +19,19 @@ class Activity201 : AppCompatActivity() {
         setContentView(R.layout.activity_201)
 
         button201.setOnClickListener {
-            var intent = Intent(this, Activity202::class.java)
-            startActivity(intent)
+            jump()
         }
+    }
+
+    private fun jump() {
+        var intent = Intent(this, Activity202::class.java)
+
+        val transTextView = textView201
+
+        val textPair = Pair.create(transTextView as View, "tTextView200")
+
+        val option = ActivityOptionsCompat.makeSceneTransitionAnimation(this, textPair);
+
+        ActivityCompat.startActivity(this, intent, option.toBundle())
     }
 }
